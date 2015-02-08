b9c4e8b93ff8302b1ede572da81fcd3db544365a
Sharanu <sharanabasu@iamplus.org>
2015-02-08 06:08:13 +0530

    sfdf

--- src/com/olaappathon/sharehack/sharehack/FragmentSearchRide.java
+++ src/com/olaappathon/sharehack/sharehack/FragmentSearchRide.java
@@ -1,19 +1,33 @@
 package com.olaappathon.sharehack.sharehack;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -21,24 +35,36 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.olaappathon.sharehack.sharehack.data.RideData;
 import com.olaappathon.sharehack.sharehack.utils.Constant;
 import com.olaappathon.sharehack.sharehack.utils.Utils;
 
-public class FragmentSearchRide extends Fragment {
+public class FragmentSearchRide extends Fragment implements GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerDragListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
 	private static Context mContext;
 	private static RideData mRideData;
@@ -51,7 +77,6 @@ public class FragmentSearchRide extends Fragment {
 
 	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 	static final LatLng KIEL = new LatLng(53.551, 9.993);
-	private GoogleMap map;
 
 	public FragmentSearchRide() {
 	}
@@ -117,13 +142,8 @@ public class FragmentSearchRide extends Fragment {
 
 		mEditTextDestination = (EditText) view.findViewById(R.id.edittext_destination);
 		// mEditTextSource = (EditText) view.findViewById(R.id.edittext_source);
+		onCreateInit();
 
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map_view_search)).getMap();
-
-		if (map != null) {
-			Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Ola"));
-			Marker kiel = map.addMarker(new MarkerOptions().position(KIEL).title("Ola").snippet("Ola is cool").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
-		}
 	}
 
 	private static void showpDialog() {
@@ -277,5 +297,395 @@ public class FragmentSearchRide extends Fragment {
 			return v;
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    protected final static String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+    protected final static String LOCATION_KEY = "location-key";
+    protected final static String LAST_UPDATED_TIME_STRING_KEY = "last-updated-time-string-key";
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+    protected Location mCurrentLocation;
+    protected Boolean mRequestingLocationUpdates;
+
+    final int RQS_GooglePlayServices = 1;
+    boolean markerClicked;
+    TextView tvLocInfo;
+    PolygonOptions polygonOptions;
+    Polygon polygon;
+    Location myLocation;
+    private GoogleMap map;
+    protected String mLastUpdateTime;
+    private Marker currentLocMarker;
+    private Marker destLocMarker;
+    private LatLng currentLatLng;
+    private int markerCount = 0;
+    List<Polyline> polylines = new ArrayList<Polyline>();
+
+    
+    protected void onCreateInit() {
+
+        buildGoogleApiClient();
+        mRequestingLocationUpdates = true;
+        try {
+	        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map_view))
+	                .getMap();
+        } catch (Exception e) {
+	        e.printStackTrace();
+        }
+        if (map != null) {
+            map.setMyLocationEnabled(true);
+            map.setOnMapClickListener(this);
+            map.setOnMapLongClickListener(this);
+            map.setOnMarkerDragListener(this);
+
+            markerClicked = false;
+        }
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        createLocationRequest();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mGoogleApiClient.connect();
+    }
+    
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        if (mGoogleApiClient.isConnected()) {
+//            mGoogleApiClient.disconnect();
+//        }
+//    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        stopLocationUpdates();
+//    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mCurrentLocation != null) {
+            currentLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+
+            placeCurrentMarker(currentLatLng, true);
+            markerCount++;
+
+            tvLocInfo.setText(String.valueOf(mCurrentLocation.getLatitude()) + " " + String.valueOf(mCurrentLocation.getLongitude()));
+        } else {
+            dealWithNoLocation(getActivity());
+        }
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void placeCurrentMarker(LatLng currentLatLng, boolean moveCamera) {
+        if(currentLocMarker == null){
+            //currentLocMarker.remove();
+            currentLocMarker = map.addMarker(new MarkerOptions().position(currentLatLng).draggable(true)
+                    .title("Your location").snippet("you are cool"));
+        }
+
+        if(moveCamera){
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+
+            // Zoom in, animating the camera.
+            map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        mGoogleApiClient.reconnect();
+        map.clear();
+        markerCount = 0;
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        mGoogleApiClient.connect();
+        map.clear();
+        markerCount = 0;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        //Toast.makeText(this, "on location changed", Toast.LENGTH_SHORT).show();
+        //placeCurrentMarker(currentLatLng, false);
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        tvLocInfo.setText(point.toString());
+        map.animateCamera(CameraUpdateFactory.newLatLng(point));
+
+        markerClicked = false;
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        markerCount++;
+        if(markerCount <=2){
+            tvLocInfo.setText("New marker added@" + point.toString());
+            destLocMarker = map.addMarker(new MarkerOptions()
+                    .position(point)
+                    .draggable(true));
+        }
+
+        if(markerCount == 2){
+            redrawRoute();
+        }
+
+        markerClicked = false;
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        tvLocInfo.setText("Marker " + marker.getId() + " Drag@" + marker.getPosition());
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        tvLocInfo.setText("Marker " + marker.getId() + " DragEnd");
+        redrawRoute();
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        tvLocInfo.setText("Marker " + marker.getId() + " DragStart");
+
+    }
+
+    private void dealWithNoLocation(final Context context){
+        LocationManager lm = null;
+        boolean gps_enabled = false,network_enabled = false;
+        if(lm==null)
+            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        try{
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch(Exception ex){}
+        try{
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }catch(Exception ex){}
+
+        if(!gps_enabled && !network_enabled){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setMessage("no location");
+            dialog.setPositiveButton("switch on", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    context.startActivity(myIntent);
+                }
+            });
+            dialog.setNegativeButton("nah", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+            dialog.show();
+
+        }else{
+            Toast.makeText(context, "shit happens", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+
+    public String makeURL (double sourcelat, double sourcelog, double destlat, double destlog ){
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("http://maps.googleapis.com/maps/api/directions/json");
+        urlString.append("?origin=");// from
+        urlString.append(Double.toString(sourcelat));
+        urlString.append(",");
+        urlString
+                .append(Double.toString( sourcelog));
+        urlString.append("&destination=");// to
+        urlString
+                .append(Double.toString( destlat));
+        urlString.append(",");
+        urlString.append(Double.toString( destlog));
+        urlString.append("&sensor=false&mode=driving&alternatives=false");
+
+        return urlString.toString();
+    }
+
+    public void drawPath(String  result) {
+
+        try {
+            //Tranform the string into a json object
+            final JSONObject json = new JSONObject(result);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routes = routeArray.getJSONObject(0);
+            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            String encodedString = overviewPolylines.getString("points");
+            List<LatLng> list = decodePoly(encodedString);
+
+            for(int z = 0; z<list.size()-1;z++){
+                LatLng src= list.get(z);
+                LatLng dest= list.get(z+1);
+                Polyline route = map.addPolyline(new PolylineOptions()
+                        .add(new LatLng(src.latitude, src.longitude), new LatLng(dest.latitude,   dest.longitude))
+                        .width(2)
+                        .color(Color.BLUE).geodesic(true));
+
+                polylines.add(route);
+            }
+        }
+        catch (JSONException e) {
+
+        }
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+    private class connectAsyncTask extends AsyncTask<Void, Void, String> {
+        private ProgressDialog progressDialog;
+        String url;
+        connectAsyncTask(String urlPass){
+            url = urlPass;
+        }
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("Fetching route, Please wait...");
+            progressDialog.setIndeterminate(true);
+            progressDialog.show();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            JSONParser jParser = new JSONParser();
+            String json = jParser.getJSONFromUrl(url);
+            return json;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.hide();
+            if(result!=null){
+                clearLinesOnMap();
+                drawPath(result);
+            }
+        }
+    }
+
+    private void redrawRoute(){
+        if(currentLatLng != null && destLocMarker != null){
+            String url = makeURL(currentLatLng.latitude, currentLatLng.longitude, destLocMarker.getPosition().latitude, destLocMarker.getPosition().longitude);
+            new connectAsyncTask(url).execute();
+        }
+    }
+
+    private void clearLinesOnMap(){
+        for(Polyline line : polylines)
+        {
+            line.remove();
+        }
+
+        polylines.clear();
+    }
+
+	
+	
 
 }
